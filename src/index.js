5027f793e60b256691c6587c188fdec179a7431c
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-11-10 17:11:48 -0500

    Working sample created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-domo";
+import ReactDOM from "react-dom";
 import App from "./App";
 
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
-rootElement.render(<App />, rootElement);
+ReactDOM.render(<App />, rootElement);
